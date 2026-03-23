0e106464fbde24c37cedbfa9cb1abaaa7e9c1f70
serkis <serkis397@mail.ru>
2026-03-24 01:27:46 +0500

    fix: replace python3 with python

--- pipeline.sh
+++ pipeline.sh
@@ -3,16 +3,16 @@
 echo "Запуск конвейера машинного обучения"
 
 echo "Генерируем данные"
-python3 data_creation.py
+python data_creation.py
 
 echo "Производим предобработку данных"
-python3 model_preprocessing.py
+python model_preprocessing.py
 
 echo "Обучаем модели"
-python3 model_preparation.py
+python model_preparation.py
 
 echo "Проводим тестирование"
-python3 model_testing.py
+python model_testing.py
 
 echo "Работа конвейера завершена!"
 read -p "Нажмите Enter для выхода..."
